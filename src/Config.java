f3aafa6f23617be6522253fdad5378c6b3b6da8a
simon.vonroenn <simon.vonroenn@web.de>
2022-09-03 23:43:31 +0200

    Add config file with starting position as constant

---
+++ src/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    private Config() {}
+
+    /** The starting position */
+    public static final String FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+}
